bbd7c6511978d53fdc8006c33b2190887d4c1132
Naresh123se <naresh132na@gmail.com>
2025-03-03 21:57:12 +0545

    update

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
         navigate('/admin')
       }
       else if (res.user.role === 'distributor'){
-        navigate('/dashboard')
+        navigate('/distributor')
       }
       toast.success("Login Successful");
     } catch (error) {
