df958ba829e252ca9b3a452a5b9467bce6a7246e
Naresh123se <naresh132na@gmail.com>
2025-03-17 19:55:45 +0545

    update my orders

--- client/src/components/Dashboard/ShopOrders.jsx
+++ client/src/components/Dashboard/ShopOrders.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useGetOrdersShopQuery } from "@/app/slices/orderApiSlice";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
+import { ScrollArea } from "../ui/scroll-area";
 
 const ShopOrders = () => {
   const { data, refetch } = useGetOrdersShopQuery();
@@ -68,7 +69,7 @@ const ShopOrders = () => {
   const displayOrders = getFilteredByTime(filteredOrders);
 
   return (
-    <div className="min-h-screen bg-gray-50 p-8">
+    <div className=" bg-gray-50 p-8">
       <div className="max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold mb-8 text-gray-900">My Orders</h1>
 
@@ -109,107 +110,108 @@ const ShopOrders = () => {
             ))}
           </select>
         </div>
-
-        {displayOrders.length > 0 ? (
-          displayOrders.map((order) => (
-            <div
-              key={order._id}
-              className="bg-white rounded-lg shadow-md border p-6 mb-6 hover:shadow-lg transition-shadow duration-300"
-            >
-              <div className="flex justify-between items-start mb-6">
-                <div className="flex items-center">
-                  <span className="font-semibold text-gray-800">
-                    Order #{order._id.slice(-6)}
+        <ScrollArea className="h-[calc(100vh-360px)]">
+          {displayOrders.length > 0 ? (
+            displayOrders.map((order) => (
+              <div
+                key={order._id}
+                className="bg-white rounded-lg shadow-md border p-6 mb-6 hover:shadow-lg transition-shadow duration-300"
+              >
+                <div className="flex justify-between items-start mb-6">
+                  <div className="flex items-center">
+                    <span className="font-semibold text-gray-800">
+                      Order #{order._id.slice(-6)}
+                    </span>
+                  </div>
+                  <span
+                    className={`px-3 py-1 rounded-full text-sm font-medium ${
+                      order.status === "pending"
+                        ? "text-yellow-700 bg-yellow-100"
+                        : order.isDelivered
+                        ? "text-green-700 bg-green-100"
+                        : "text-blue-700 bg-blue-100"
+                    }`}
+                  >
+                    {order.status}
+                    {order.isDelivered ? " (Delivered)" : ""}
                   </span>
                 </div>
-                <span
-                  className={`px-3 py-1 rounded-full text-sm font-medium ${
-                    order.status === "pending"
-                      ? "text-yellow-700 bg-yellow-100"
-                      : order.isDelivered
-                      ? "text-green-700 bg-green-100"
-                      : "text-blue-700 bg-blue-100"
-                  }`}
-                >
-                  {order.status}
-                  {order.isDelivered ? " (Delivered)" : ""}
-                </span>
-              </div>
 
-              <div className="space-y-6">
-                {order.orderItems?.map((item) => (
-                  <div key={item._id} className="flex gap-6">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="w-24 h-24 object-cover rounded-lg"
-                    />
+                <div className="space-y-6">
+                  {order.orderItems?.map((item) => (
+                    <div key={item._id} className="flex gap-6">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="w-24 h-24 object-cover rounded-lg"
+                      />
 
-                    <div className="flex-1 grid grid-cols-2 md:grid-cols-4 gap-6">
-                      <div>
-                        <h4 className="font-semibold text-gray-900">
-                          {item.name}
-                        </h4>
-                        <p className="text-sm text-gray-500">
-                          Payment: {order.paymentMethod}
-                        </p>
-                      </div>
-
-                      <div>
-                        <p className="text-sm text-gray-500">Price</p>
-                        <p className="text-gray-900 font-medium">
-                          ₹{item.price?.toFixed(2)}
-                        </p>
-                      </div>
-
-                      <div>
-                        <p className="text-sm text-gray-500">Tax</p>
-                        <p className="text-gray-900">
-                          ₹{order.taxPrice.toFixed(2)}
-                        </p>
-                      </div>
-
-                      <div>
-                        <p className="text-sm text-gray-500">Total</p>
-                        <p className="text-gray-900 font-semibold">
-                          ₹{order.totalPrice.toFixed(2)}
-                        </p>
-                      </div>
-
-                      <div className="col-span-2 md:col-span-4">
-                        <p className="text-sm text-gray-500">
-                          Quantity: {item.qty}
-                        </p>
-                        <hr className="my-2" />
-                        <div className="flex items-center gap-2">
-                          <p className="text-sm text-gray-500 ">
-                            Shipping Address:
+                      <div className="flex-1 grid grid-cols-2 md:grid-cols-4 gap-6">
+                        <div>
+                          <h4 className="font-semibold text-gray-900">
+                            {item.name}
+                          </h4>
+                          <p className="text-sm text-gray-500">
+                            Payment: {order.paymentMethod}
                           </p>
+                        </div>
+
+                        <div>
+                          <p className="text-sm text-gray-500">Price</p>
+                          <p className="text-gray-900 font-medium">
+                            ₹{item.price?.toFixed(2)}
+                          </p>
+                        </div>
+
+                        <div>
+                          <p className="text-sm text-gray-500">Tax</p>
                           <p className="text-gray-900">
-                            {order.shippingAddress.address},{" "}
-                            {order.shippingAddress.city},{" "}
-                            {order.shippingAddress.postalCode},{" "}
-                            {order.shippingAddress.country}
+                            ₹{order.taxPrice.toFixed(2)}
                           </p>
+                        </div>
+
+                        <div>
+                          <p className="text-sm text-gray-500">Total</p>
+                          <p className="text-gray-900 font-semibold">
+                            ₹{order.totalPrice.toFixed(2)}
+                          </p>
+                        </div>
+
+                        <div className="col-span-2 md:col-span-4">
+                          <p className="text-sm text-gray-500">
+                            Quantity: {item.qty}
+                          </p>
+                          <hr className="my-2" />
+                          <div className="flex items-center gap-2">
+                            <p className="text-sm text-gray-500 ">
+                              Shipping Address:
+                            </p>
+                            <p className="text-gray-900">
+                              {order.shippingAddress.address},{" "}
+                              {order.shippingAddress.city},{" "}
+                              {order.shippingAddress.postalCode},{" "}
+                              {order.shippingAddress.country}
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <div className="bg-white rounded-lg shadow-md border p-8 text-center flex flex-col text-gray-500 gap-3">
-            <p>There are no orders placed yet.</p>
+            ))
+          ) : (
+            <div className="bg-white rounded-lg shadow-md border p-8 text-center flex flex-col text-gray-500 gap-3">
+              <p>There are no orders placed yet.</p>
 
-            <p>
-              <Button onClick={() => nav("/dashboard")}>
-                CONTINUE SHOPPING
-              </Button>
-            </p>
-          </div>
-        )}
+              <p>
+                <Button onClick={() => nav("/dashboard")}>
+                  CONTINUE SHOPPING
+                </Button>
+              </p>
+            </div>
+          )}
+        </ScrollArea>
       </div>
     </div>
   );
